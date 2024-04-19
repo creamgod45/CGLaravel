13530ff2b937fb2fb17a4e982ea0233b6813849b
creamgod45 <fuyin1054@gmail.com>
2024-04-19 22:46:53 +0800

    version 1.0.0

--- local.bat
+++ local.bat
@@ -1,8 +1,6 @@
 @echo off
 composer install --optimize-autoloader
-php artisan config:clear
-php artisan route:clear
-php artisan view:clear
+php artisan optimize:clear
 php artisan key:generate
 php artisan optimize
 php artisan config:clear &&  composer dump-autoload -o
