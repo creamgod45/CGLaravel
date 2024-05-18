f5408b2a4c5ba1c4d1d33de03e5d4b4cbb160fe2
creamgod45 <fuyin1054@gmail.com>
2024-05-19 03:48:54 +0800

    新增 關閉服務功能 新增 允許旁路呼叫維護模式API 更新 README.md

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\AnimalController;
 use App\Http\Middleware\EMiddleWareAliases;
+use App\Http\Middleware\PreventRequestsDuringMaintenance;
 use Illuminate\Http\Request;
 //use Illuminate\Support\Facades\Cookie;
 use Illuminate\Support\Facades\Route;
@@ -23,3 +24,28 @@ Route::middleware(EMiddleWareAliases::auth->name)->get('/user', function (Reques
 
 Route::apiResource('animal', AnimalController::class);
 
+Route::get('down', function (Request $request){
+    if (Hash::check($request['key'], '$2y$12$odnzjr7ZJSFqOdkAQrDMzuPY6sHoN6n8aoTozqHYsgojw2A5oyAT6')) {
+        if (App::isDownForMaintenance()) {
+            App::maintenanceMode()->deactivate();
+            return response()->json([
+                "message" => "turn off maintenance Mode"
+            ]);
+        }else{
+            $random = Str::random("60");
+            App::maintenanceMode()->activate([
+                'except' => App::make(PreventRequestsDuringMaintenance::class)->getExcludedPaths(),
+                'redirect' => "/",
+                'retry' => 60,
+                'refresh' => 15,
+                'secret' => $random,
+                'status' => 503,
+                'template' => null,
+            ]);
+            return response()->json([
+                "message" => "turn on maintenance Mode",
+                "token" => $random
+            ]);
+        }
+    }
+});
