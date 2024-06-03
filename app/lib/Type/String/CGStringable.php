6df88919109e5d40ee64bfca65146dc0fffe2666
creamgod45 <fuyin1054@gmail.com>
2024-06-04 01:15:59 +0800

    新增 CGStringable.php 始物件或是變數嘗試轉換成字串結果用於除錯與Log資訊提供

---
+++ app/lib/Type/String/CGStringable.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Lib\Type\String;
+
+use Exception;
+use Psy\Util\Json;
+
+class CGStringable
+{
+    private $string;
+    /**
+     * @throws Exception
+     */
+    public function __construct(mixed $variable)
+    {
+        if (is_array($variable)) {
+            $this->string = Json::encode($variable);
+        } elseif (is_object($variable)) {
+            $this->string = serialize($variable);
+        }elseif (is_bool($variable)) {
+            if($variable){
+                $this->string = 'true';
+            }else{
+                $this->string = 'false';
+            }
+        } elseif (is_numeric($variable)) {
+            $this->string = (string) $variable;
+        }  elseif (is_double($variable)) {
+            $this->string = (string) $variable;
+        } elseif (is_float($variable)) {
+            $this->string = (string) $variable;
+        } else {
+            throw new Exception("Not supported converter Stringable variable", 0);
+        }
+    }
+
+    public function toString(){
+        return $this->string;
+    }
+
+    public function toCGString(): CGString
+    {
+        return new CGString($this->string);
+    }
+
+    public function __toString(): string
+    {
+        return $this->string;
+    }
+}
