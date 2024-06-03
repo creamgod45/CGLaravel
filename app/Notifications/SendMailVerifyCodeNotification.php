ab1c4078bc7a06c0e0ca81cd617155c1664276cb
creamgod45 <fuyin1054@gmail.com>
2024-06-04 01:19:38 +0800

    更新 Log 資訊紀錄便於除錯

--- app/Notifications/SendMailVerifyCodeNotification.php
+++ app/Notifications/SendMailVerifyCodeNotification.php
@@ -4,10 +4,12 @@ namespace App\Notifications;
 
 use App\Lib\I18N\ELanguageText;
 use App\Lib\I18N\I18N;
+use App\Lib\Type\String\CGStringable;
 use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Notifications\Messages\MailMessage;
 use Illuminate\Notifications\Notification;
+use Illuminate\Support\Facades\Log;
 
 class SendMailVerifyCodeNotification extends Notification implements ShouldQueue
 {
@@ -24,6 +26,12 @@ class SendMailVerifyCodeNotification extends Notification implements ShouldQueue
 
     public function toMail($notifiable): MailMessage
     {
+        Log::info("=================================================================================================");
+        Log::info("Proccess: VerifyEmailNotification");
+        Log::info("Debug \$Instance: ".(new CGStringable($this)));
+        Log::info("Debug \$i18N: ".$this->i18N->getLanguageCode()->name);
+        Log::info("Debug \$notifiable: ".(new CGStringable($notifiable)));
+        Log::info("=================================================================================================");
         return (new MailMessage)
             ->subject($this->i18N->getLanguage(ELanguageText::sendMailVerifyCode_Response_error1))
             ->line($this->i18N->getLanguage(ELanguageText::sendMailVerifyCodeLine1, true)->placeholderParser("code", $this->code)->toString())
--- app/Notifications/VerifyEmailNotification.php
+++ app/Notifications/VerifyEmailNotification.php
@@ -4,11 +4,15 @@ namespace App\Notifications;
 
 use App\Lib\I18N\ELanguageText;
 use App\Lib\I18N\I18N;
+use App\Lib\Type\String\CGStringable;
+use App\Lib\Utils\Utilsv2;
 use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Notifications\Messages\MailMessage;
 use Illuminate\Notifications\Notification;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\URL;
+use Exception;
 
 class VerifyEmailNotification extends Notification implements ShouldQueue
 {
@@ -38,21 +42,27 @@ class VerifyEmailNotification extends Notification implements ShouldQueue
     public function toMail(object $notifiable): MailMessage
     {
         $verificationUrl = $this->verificationUrl($notifiable);
-
-        return (new MailMessage)
-            ->subject($this->i18N->getLanguage(ELanguageText::RegisterMailTitle))
-            ->line($this->i18N->getLanguage(ELanguageText::RegisterMailLine1))
-            ->action($this->i18N->getLanguage(ELanguageText::RegisterMailAction1), $verificationUrl)
-            ->line($this->i18N->getLanguage(ELanguageText::RegisterMailLine2));
+        Log::info("=================================================================================================");
+        Log::info("Proccess: VerifyEmailNotification");
+        try {
+            Log::info("Debug \$Instance: (encode)" . Utilsv2::encodeContext((new CGStringable($this))));
+        } catch (Exception $e) {
+            Log::warning($e->getMessage());
+        }
+        Log::info("Debug \$i18N: " . $this->i18N->getLanguageCode()->name);
+        Log::info("Debug \$verificationUrl: " . $verificationUrl);
+        try {
+            Log::info("Debug \$notifiable: (encode)" . Utilsv2::encodeContext((new CGStringable($notifiable))));
+        } catch (Exception $e) {
+            Log::warning($e->getMessage());
+        }
+        Log::info("=================================================================================================");
+        return (new MailMessage)->subject($this->i18N->getLanguage(ELanguageText::RegisterMailTitle))->line($this->i18N->getLanguage(ELanguageText::RegisterMailLine1))->action($this->i18N->getLanguage(ELanguageText::RegisterMailAction1), $verificationUrl)->line($this->i18N->getLanguage(ELanguageText::RegisterMailLine2));
     }
 
     protected function verificationUrl($notifiable)
     {
-        return URL::temporarySignedRoute(
-            'verification.verify',
-            now()->addSeconds(5),
-            ['id' => $notifiable->getKey(), 'hash' => sha1($notifiable->getEmailForVerification())]
-        );
+        return URL::temporarySignedRoute('verification.verify', now()->addSeconds(5), ['id' => $notifiable->getKey(), 'hash' => sha1($notifiable->getEmailForVerification())]);
     }
 
     /**
@@ -62,8 +72,7 @@ class VerifyEmailNotification extends Notification implements ShouldQueue
      */
     public function toArray(object $notifiable): array
     {
-        return [
-            //
+        return [//
         ];
     }
 }
