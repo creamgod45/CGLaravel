72b6296aed0e4f2c5aaed8d12b617efae2cd0c76
creamgod45 <fuyin1054@gmail.com>
2024-06-04 23:53:42 +0800

    更新 移除無用代碼

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -17,12 +17,12 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
 import Echo from 'laravel-echo';
 
-import Pusher from 'pusher-js';
-
-// Enable pusher logging - don't include this in production
+//import Pusher from 'pusher-js';
+//
+////Enable pusher logging - don't include this in production
 //Pusher.logToConsole = true;
 //window.Pusher = Pusher;
-
+//
 //window.Echo = new Echo({
 //    broadcaster: 'pusher',
 //    key: import.meta.env.VITE_PUSHER_APP_KEY,
