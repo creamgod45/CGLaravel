10f40bd661235130112d4ebf4c143ad47afd5fba
creamgod45 <fuyin1054@gmail.com>
2024-06-05 23:12:37 +0800

    更新 bootstrap.js 解決問題

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -4,10 +4,10 @@
  * CSRF token as a header based on the value of the "XSRF" token cookie.
  */
 
-import axios from 'axios';
-window.axios = axios;
+//import axios from 'axios';
+//window.axios = axios;
 
-window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+//.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
 /**
  * Echo exposes an expressive API for subscribing to channels and listening
@@ -15,7 +15,7 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
  * allows your team to easily build robust real-time web applications.
  */
 
-import Echo from 'laravel-echo';
+//import Echo from 'laravel-echo';
 
 //import Pusher from 'pusher-js';
 //
