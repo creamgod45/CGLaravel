8add414ae714b567477bfe304c7d7004d95f7c5f
creamgod45 <fuyin1054@gmail.com>
2024-06-05 23:05:06 +0800

    更新 ENotificationType.php 添加 valueof isVaild 邏輯函式

--- app/lib/Utils/ENotificationType.php
+++ app/lib/Utils/ENotificationType.php
@@ -2,10 +2,32 @@
 
 namespace App\Lib\Utils;
 
+use App\Lib\Type\String\CGString;
+
 enum ENotificationType
 {
     case info;
     case warning;
     case error;
     case success;
+
+    public static function isVaild(string $name):bool
+    {
+        foreach (ENotificationType::cases() as $case) {
+            if ((new CGString($case->name))->toUpperCase()->toString() === (new CGString($name))->toUpperCase()->toString()) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static function valueof(string $name): ?ENotificationType
+    {
+        foreach (ENotificationType::cases() as $case) {
+            if ((new CGString($case->name))->toUpperCase()->toString() === (new CGString($name))->toUpperCase()->toString()) {
+                return $case;
+            }
+        }
+        return null;
+    }
 }
