72a4848b8ffd297953f4e21727bcedb238d63943
creamgod45 <fuyin1054@gmail.com>
2024-06-10 01:20:04 +0800

    更新 api.php use

--- routes/api.php
+++ routes/api.php
@@ -4,8 +4,10 @@ use App\Http\Controllers\AnimalController;
 use App\Http\Middleware\EMiddleWareAliases;
 use App\Http\Middleware\PreventRequestsDuringMaintenance;
 use Illuminate\Http\Request;
-//use Illuminate\Support\Facades\Cookie;
+use Illuminate\Support\Facades\App;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Str;
 
 /*
 |--------------------------------------------------------------------------
