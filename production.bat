16b08e5c42cfc97ec15575f6699b37fdf2143751
creamgod45 <fuyin1054@gmail.com>
2024-07-02 20:35:56 +0800

    更新 產品化腳本

--- production.bat
+++ production.bat
@@ -1,16 +1,17 @@
 REM config/app.php debugbar comment
 REM public copy to root
-REM Remove all hot file is laravel dected local server or server assets 
+REM Remove all hot file is laravel dected local server or server assets
 REM DONT UPLOAD .env.* will can be download file
 REM DONT UPLOAD Unless File
 @echo off
 composer install --optimize-autoloader --no-dev
+php artisan key:generate
+php artisan optimize:clear
+php artisan config:clear &&  composer dump-autoload -o
 php artisan config:cache
 php artisan route:cache
 php artisan view:cache
-php artisan key:generate
 php artisan optimize
-php artisan config:clear &&  composer dump-autoload -o
 npm install
 npm run build
 echo Done
