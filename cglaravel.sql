da9db01f9e87a5761b12b0b04654cbedb39f5ad3
creamgod45 <fuyin1054@gmail.com>
2024-07-02 20:37:08 +0800

    更新 根 cglaravel.sql

--- cglaravel.sql
+++ cglaravel.sql
@@ -1,133 +1,108 @@
-create table if not exists animals
-(
-    id          bigint unsigned auto_increment
-        primary key,
-    type_id     int unsigned         not null comment '動物分類',
-    name        varchar(255)         not null comment '動物的暱稱',
-    birthday    date                 null comment '生日',
-    area        varchar(255)         null comment '所在地區',
-    fix         tinyint(1) default 0 not null comment '結紮情形',
-    description text                 null comment '簡單敘述',
-    personality text                 null comment '動物個性',
-    created_at  timestamp            null,
-    updated_at  timestamp            null
-)
-    collate = utf8mb4_unicode_ci;
+-- animals: table
+CREATE TABLE `animals` (
+                           `id` bigint unsigned NOT NULL AUTO_INCREMENT,
+                           `type_id` int unsigned NOT NULL COMMENT '動物分類',
+                           `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL COMMENT '動物的暱稱',
+                           `birthday` date DEFAULT NULL COMMENT '生日',
+                           `area` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL COMMENT '所在地區',
+                           `fix` tinyint(1) NOT NULL DEFAULT '0' COMMENT '結紮情形',
+                           `description` text COLLATE utf8mb4_unicode_ci COMMENT '簡單敘述',
+                           `personality` text COLLATE utf8mb4_unicode_ci COMMENT '動物個性',
+                           `created_at` timestamp NULL DEFAULT NULL,
+                           `updated_at` timestamp NULL DEFAULT NULL,
+                           PRIMARY KEY (`id`),
+                           UNIQUE KEY `animals_type_id_unique` (`type_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
-alter table animals
-    add constraint animals_type_id_unique
-        unique (type_id);
+-- jobs: table
+CREATE TABLE `jobs` (
+                        `id` bigint unsigned NOT NULL AUTO_INCREMENT,
+                        `queue` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+                        `payload` longtext COLLATE utf8mb4_unicode_ci NOT NULL,
+                        `attempts` tinyint unsigned NOT NULL,
+                        `reserved_at` int unsigned DEFAULT NULL,
+                        `available_at` int unsigned NOT NULL,
+                        `created_at` int unsigned NOT NULL,
+                        PRIMARY KEY (`id`),
+                        KEY `jobs_queue_index` (`queue`)
+) ENGINE=InnoDB AUTO_INCREMENT=491 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
-create table if not exists jobs
-(
-    id           bigint unsigned auto_increment
-        primary key,
-    queue        varchar(255)     not null,
-    payload      longtext         not null,
-    attempts     tinyint unsigned not null,
-    reserved_at  int unsigned     null,
-    available_at int unsigned     not null,
-    created_at   int unsigned     not null
-)
-    collate = utf8mb4_unicode_ci;
+-- No native definition for element: jobs_queue_index (index)
 
-create index jobs_queue_index
-    on jobs (queue);
+-- members: table
+CREATE TABLE `members` (
+                           `id` bigint unsigned NOT NULL AUTO_INCREMENT,
+                           `UUID` char(36) COLLATE utf8mb4_unicode_ci NOT NULL,
+                           `username` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+                           `email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+                           `email_verified_at` timestamp NULL DEFAULT NULL,
+                           `password` text COLLATE utf8mb4_unicode_ci NOT NULL,
+                           `phone` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+                           `enable` enum('false','true') COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT 'false',
+                           `administrator` enum('false','true') COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT 'false',
+                           `remember_token` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+                           `created_at` timestamp NULL DEFAULT NULL,
+                           `updated_at` timestamp NULL DEFAULT NULL,
+                           PRIMARY KEY (`id`),
+                           UNIQUE KEY `members_username_unique` (`username`),
+                           UNIQUE KEY `members_email_unique` (`email`),
+                           UNIQUE KEY `members_phone_unique` (`phone`)
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
-create table if not exists members
-(
-    id                bigint unsigned auto_increment
-        primary key,
-    UUID              char(36)                               not null,
-    username          varchar(255)                           not null,
-    email             varchar(255)                           not null,
-    email_verified_at timestamp                              null,
-    password          text                                   not null,
-    phone             varchar(255)                           not null,
-    enable            enum ('false', 'true') default 'false' not null,
-    administrator     enum ('false', 'true') default 'false' not null,
-    remember_token    varchar(100)                           null,
-    created_at        timestamp                              null,
-    updated_at        timestamp                              null
-)
-    collate = utf8mb4_unicode_ci;
+-- migrations: table
+CREATE TABLE `migrations` (
+                              `id` int unsigned NOT NULL AUTO_INCREMENT,
+                              `migration` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+                              `batch` int NOT NULL,
+                              PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
-alter table members
-    add constraint members_email_unique
-        unique (email);
+-- password_reset_tokens: table
+CREATE TABLE `password_reset_tokens` (
+                                         `id` bigint unsigned NOT NULL AUTO_INCREMENT,
+                                         `email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+                                         `token` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+                                         `created_at` timestamp NULL DEFAULT NULL,
+                                         `updated_at` timestamp NULL DEFAULT NULL,
+                                         PRIMARY KEY (`id`),
+                                         KEY `password_reset_tokens_email_index` (`email`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
-alter table members
-    add constraint members_phone_unique
-        unique (phone);
+-- No native definition for element: password_reset_tokens_email_index (index)
 
-alter table members
-    add constraint members_username_unique
-        unique (username);
+-- personal_access_tokens: table
+CREATE TABLE `personal_access_tokens` (
+                                          `id` bigint unsigned NOT NULL AUTO_INCREMENT,
+                                          `tokenable_type` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+                                          `tokenable_id` bigint unsigned NOT NULL,
+                                          `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+                                          `token` varchar(64) COLLATE utf8mb4_unicode_ci NOT NULL,
+                                          `abilities` text COLLATE utf8mb4_unicode_ci,
+                                          `last_used_at` timestamp NULL DEFAULT NULL,
+                                          `expires_at` timestamp NULL DEFAULT NULL,
+                                          `created_at` timestamp NULL DEFAULT NULL,
+                                          `updated_at` timestamp NULL DEFAULT NULL,
+                                          PRIMARY KEY (`id`),
+                                          UNIQUE KEY `personal_access_tokens_token_unique` (`token`),
+                                          KEY `personal_access_tokens_tokenable_type_tokenable_id_index` (`tokenable_type`,`tokenable_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
-create table if not exists migrations
-(
-    id        int unsigned auto_increment
-        primary key,
-    migration varchar(255) not null,
-    batch     int          not null
-)
-    collate = utf8mb4_unicode_ci;
+-- No native definition for element: personal_access_tokens_tokenable_type_tokenable_id_index (index)
 
-create table if not exists password_reset_tokens
-(
-    id         bigint unsigned auto_increment
-        primary key,
-    email      varchar(255) not null,
-    token      varchar(255) not null,
-    created_at timestamp    null,
-    updated_at timestamp    null
-)
-    collate = utf8mb4_unicode_ci;
+-- sessions: table
+CREATE TABLE `sessions` (
+                            `id` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+                            `user_id` bigint unsigned DEFAULT NULL,
+                            `ip_address` varchar(45) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+                            `user_agent` text COLLATE utf8mb4_unicode_ci,
+                            `payload` longtext COLLATE utf8mb4_unicode_ci NOT NULL,
+                            `last_activity` int NOT NULL,
+                            PRIMARY KEY (`id`),
+                            KEY `sessions_user_id_index` (`user_id`),
+                            KEY `sessions_last_activity_index` (`last_activity`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
-create index password_reset_tokens_email_index
-    on password_reset_tokens (email);
+-- No native definition for element: sessions_user_id_index (index)
 
-create table if not exists personal_access_tokens
-(
-    id             bigint unsigned auto_increment
-        primary key,
-    tokenable_type varchar(255)    not null,
-    tokenable_id   bigint unsigned not null,
-    name           varchar(255)    not null,
-    token          varchar(64)     not null,
-    abilities      text            null,
-    last_used_at   timestamp       null,
-    expires_at     timestamp       null,
-    created_at     timestamp       null,
-    updated_at     timestamp       null
-)
-    collate = utf8mb4_unicode_ci;
-
-create index personal_access_tokens_tokenable_type_tokenable_id_index
-    on personal_access_tokens (tokenable_type, tokenable_id);
-
-alter table personal_access_tokens
-    add constraint personal_access_tokens_token_unique
-        unique (token);
-
-create table if not exists sessions
-(
-    id            varchar(255)    not null,
-    user_id       bigint unsigned null,
-    ip_address    varchar(45)     null,
-    user_agent    text            null,
-    payload       longtext        not null,
-    last_activity int             not null
-)
-    collate = utf8mb4_unicode_ci;
-
-create index sessions_last_activity_index
-    on sessions (last_activity);
-
-create index sessions_user_id_index
-    on sessions (user_id);
-
-alter table sessions
-    add primary key (id);
-
+-- No native definition for element: sessions_last_activity_index (index)
 
