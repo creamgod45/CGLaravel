791450ebbd0eaa2dcfe9c5adea57c3622e34ebf4
creamgod45 <fuyin1054@gmail.com>
2024-07-04 22:00:09 +0800

    更新 CheckClientID.php 測試模式時繞過，驗證阻擋。

--- app/Http/Middleware/CheckClientID.php
+++ app/Http/Middleware/CheckClientID.php
@@ -5,6 +5,8 @@ namespace App\Http\Middleware;
 use App\Lib\Utils\RouteNameField;
 use Closure;
 use Illuminate\Http\Request;
+use Illuminate\Support\Env;
+use Illuminate\Support\Facades\Config;
 use Illuminate\Support\Facades\Log;
 use Symfony\Component\HttpFoundation\Response;
 
@@ -17,12 +19,11 @@ class CheckClientID
      */
     public function handle(Request $request, Closure $next): Response
     {
-        if ($request->is('hello')) {
-            return $next($request);
-        }
-
-        if (!$request->session()->has('ClientID')) {
+        if (!$request->session()->has('ClientID') && !Config::get('app.env') === "testing") {
             return redirect()->route(RouteNameField::PageGetClientID->value); // 替换为你的路由名称
+        }
+        if(Config::get('app.env') === "testing"){
+            $request->session()->put('ClientID', sha1(time()));
         }
 
         return $next($request);
